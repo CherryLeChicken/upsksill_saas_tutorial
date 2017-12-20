6fd14c462d75e73efc8a04c673d910aae3898e5f
Cherry Ke <cherryke07@gmail.com>
2017-12-20 01:10:29 +0000

    Added user profile styles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -11,6 +11,7 @@ def create
   @user = User.find( params[:user_id] )
   # Create profile linked to this specific user
   @profile = @user.build_profile( profile_params )
+  
   if @profile.save
     flash[:success] = "Profile updated!"
     redirect_to user_path( params[:user_id] )
@@ -21,7 +22,7 @@ end
 
 private
   def profile_params
-    params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+    params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
   end
   
 end
